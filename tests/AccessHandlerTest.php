b7ff43c5ef445839c7803b277ee4161f4cc28fab
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-11 10:27:41 -0500

    Merge first proposed exercise with its solution

--- tests/AccessHandlerTest.php
+++ tests/AccessHandlerTest.php
@@ -7,30 +7,84 @@ use Styde\User;
 
 class AccessHandlerTest extends PHPUnit\Framework\TestCase
 {
-    public function tearDown()
+
+    /**
+     * @var \Styde\AccessHandler
+     */
+    protected $access;
+
+    /**
+     * This method is executed BEFORE each test method
+     */
+    protected function setUp()
+    {
+        $this->access = new Access($this->getAuthenticatorMock());
+    }
+
+    /**
+     * This method is executed AFTER each test method
+     */
+    protected function tearDown()
     {
         Mockery::close();
     }
 
     public function test_grant_access()
     {
-        $access = new Access($this->getAuthenticatorMock());
-
         $this->assertTrue(
-            $access->check('admin')
+            $this->access->check('admin')
         );
     }
 
     public function test_deny_access()
     {
-        $access = new Access($this->getAuthenticatorMock());
-
         $this->assertFalse(
-            $access->check('editor')
+            $this->access->check('editor')
         );
     }
 
-    public function getAuthenticatorMock()
+    /**
+     * Exercise: make this tests pass :-)
+     *
+     * After finishing your homework, please send a pull request (PR) to:
+     *
+     * https://github.com/StydeNet/unit-tests
+     *
+     * We might not merge them but we'll check all of them
+     */
+
+    /**
+     * Should return TRUE because our is an admin
+     */
+    public function test_grant_access_with_more_than_one_role()
+    {
+        $this->assertTrue(
+            $this->access->check(['admin', 'superadmin'])
+        );
+    }
+
+    /**
+     * Should return FALSE because our user is neither and editor nor a user
+     */
+    public function test_deny_access_with_more_than_one_role()
+    {
+        $this->assertFalse(
+            $this->access->check(['editor', 'user'])
+        );
+    }
+
+    /**
+     * Should accept strings divided by " | "
+     */
+    public function test_allow_pipe_character()
+    {
+        $this->assertTrue(
+            $this->access->check('admin|superadmin'),
+            'It should accept a string divided by the pipe character " | "'
+        );
+    }
+
+    protected function getAuthenticatorMock()
     {
         $user = Mockery::mock(User::class);
         $user->role = 'admin';
@@ -39,7 +93,6 @@ class AccessHandlerTest extends PHPUnit\Framework\TestCase
         $auth->shouldReceive('check')
             ->once()
             ->andReturn(true);
-
         $auth->shouldReceive('user')
             ->once()
             ->andReturn($user);
