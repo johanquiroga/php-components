6fff5ecf4772321c666309270ae3469083f6a9f5
JohanQuiroga <quirogacj@utp.edu.co>
2018-01-12 11:00:11 -0500

    Finished 9th lesson: Creation of a more advanced Dependency Injection Container

--- src/Container.php
+++ src/Container.php
@@ -2,74 +2,40 @@
 
 namespace Styde;
 
+use Closure;
+
 class Container
 {
-    protected static $container;
+    protected $bindings = [];
 
     protected $shared = [];
 
-    public static function getInstance()
+    public function bind($name, $resolver)
     {
-        if (static::$container == null) {
-            static::$container = new Container;
-        }
-
-        return static::$container;
-    }
-
-    public static function setContainer(Container $container)
-    {
-        static::$container = $container;
-    }
-
-    public static function clearContainer()
-    {
-        static::$container = null;
-    }
-
-    protected function hasShared($name)
-    {
-        return isset($this->shared[$name]);
-    }
-
-    protected function getShared($name)
-    {
-        return $this->shared[$name];
-    }
-
-    public function session()
-    {
-        if ($this->hasShared('session')) {
-            return $this->getShared('session');
-        }
-
-        $data = [
-            'user_data' => [
-                'name' => 'Duilio',
-                'role' => 'teacher'
-            ]
+        $this->bindings[$name] = [
+            'resolver' => $resolver
         ];
-
-        $driver = new SessionArrayDriver($data);
-
-        return $this->shared['session'] = new SessionManager($driver);
     }
 
-    public function auth()
+    public function instance($name, $object)
     {
-        if ($this->hasShared('auth')) {
-            return $this->getShared('auth');
-        }
-
-        return $this->shared['auth'] = new Authenticator($this->session());
+        $this->shared[$name] = $object;
     }
 
-    public function access()
+    public function make($name)
     {
-        if ($this->hasShared('access')) {
-            return $this->getShared('access');
+        if (isset($this->shared[$name])) {
+            return $this->shared[$name];
         }
 
-        return $this->shared['access'] = new AccessHandler($this->auth());
+        $resolver = $this->bindings[$name]['resolver'];
+
+        if ($resolver instanceof Closure) {
+            $object = $resolver($this);
+        } else {
+            $object = new $resolver;
+        }
+
+        return $object;
     }
 }
